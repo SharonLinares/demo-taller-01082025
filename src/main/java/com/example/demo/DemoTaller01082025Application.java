038069da0fde827e57a99f6d77e1c3c07e992082
Sharon Linares <akiralyn02@gmail.com>
2025-08-01 14:44:25 +0200

    cree el proyecto

---
+++ src/main/java/com/example/demo/DemoTaller01082025Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTaller01082025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTaller01082025Application.class, args);
+	}
+
+}
